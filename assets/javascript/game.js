574d9e508b3ddcb34672ce301fdc5ec23870e3b7
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-02-08 21:03:16 -0800

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,17 @@
 //When Html loads, script runs
+console.log("a")
 $(document).ready(function() {
 
 var playerScore = 0;
+$("#player-score").text(playerScore);
 var clickCount = 0;
 var wins = 0;
+$("#wins").text(wins);
 var losses = 0;
+$("#losses").text(losses);
 var randomNumber = makeRandomNumber(19, 120);
+console.log(randomNumber);
+$("#randomNumber").text(randomNumber);
 var crystalNumber = makeRandomNumber(1, 12);
 
 restartGame();
@@ -16,6 +22,8 @@ function makeRandomNumber(min, max) {
 	return Math.floor(Math.random() * ((max - min) + 1) + min);
 
 }
+
+makeRandomNumber(19, 120);
 
 //Function to restart game at beginning and after win/loss
 function restartGame() {
@@ -52,19 +60,19 @@ $(".gem-button").on("click", function() {
 var crystals = {
 	sapphire: {
 		name: "Sapphire",
-		value: 0
+		value: makeRandomNumber(1,12),
 	}
 	emerald: {
 		name: "Emerald",
-		value: 0
+		value: makeRandomNumber(1,12),
 	}
 	diamond: {
 		name: "Diamond",
-		value: 0
+		value: makeRandomNumber(1,12),
 	}
 	topaz: {
 	name: "Yellow Topaz",
-	value: 0
+	value: makeRandomNumber(1,12),
 
 }
 
